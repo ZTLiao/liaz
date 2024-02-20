bf6a68a2437d57762c867f487f6ce24c14067515
liaozetao <1107136310@qq.com>
2024-02-20 12:42:33 +0800

    修改打包脚本

--- liaz-build.sh
+++ liaz-build.sh
@@ -2,4 +2,4 @@
 
 flutter clean
 
-flutter build apk --release --dart-define=CHANNEL=official --split-per-abi --no-sound-null-safety --no-enable-impeller
+flutter build apk --release --dart-define=CHANNEL=official --split-per-abi
